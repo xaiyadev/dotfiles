1cbb194a94e6df52368a5f668bf55143cb0e94d5
Danil Schumin <danil80sch@gmail.com>
2024-07-28 17:36:06 +0200

    zenless changes
    Signed-off-by: Danil Schumin <danil80sch@gmail.com>

--- modules/apps/zenlessZoneZero/default.nix
+++ modules/apps/zenlessZoneZero/default.nix
@@ -1,5 +1,14 @@
-let
+{pkgs, ...}: let
   aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
 in
 /* TODO: code to config */
-{ home.packages = [ aagl-gtk-on-nix.sleepy-launcher ]; }
+{ 
+    home.packages = with pkgs; [ 
+	# programs needed for installation
+	git
+	p7zip
+	libwebp
+
+	aagl-gtk-on-nix.sleepy-launcher 
+    ]; 
+}
