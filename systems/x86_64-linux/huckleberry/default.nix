e8a397c101864a478b273b785dba3c8f3efab034
Danil Schumin <danil80sch@gmail.com>
2024-09-10 10:40:43 +0200

    added: devenv pkgs
    Signed-off-by: Danil Schumin <danil80sch@gmail.com>

--- systems/x86_64-linux/huckleberry/default.nix
+++ systems/x86_64-linux/huckleberry/default.nix
@@ -26,6 +26,7 @@
 
   environment.systemPackages = with pkgs; [
     vim
+    devenv
   ];
 
   ${namespace} = {
--- systems/x86_64-linux/pineapple/default.nix
+++ systems/x86_64-linux/pineapple/default.nix
@@ -30,6 +30,7 @@
 
   environment.systemPackages = with pkgs; [
     vim
+    devenv
   ];
 
   ${namespace} = {
