bae3473dbf369977149f017ff54748e8512df8c0
Danil Schumin <danil80sch@gmail.com>
2024-09-30 20:43:36 +0200

    added windows devices
    Signed-off-by: Danil Schumin <danil80sch@gmail.com>

--- systems/x86_64-linux/huckleberry/hardware-configuration.nix
+++ systems/x86_64-linux/huckleberry/hardware-configuration.nix
@@ -17,8 +17,6 @@
   boot.kernelModules = [ "kvm-amd" "nouveau" ];
   boot.extraModulePackages = [ ];
 
-  # TODO: maybe add windows disk
-  # TODO: add Game disk!
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/f972b765-8131-4fc8-9004-5bea1624e0d7";
       fsType = "ext4";
@@ -30,9 +28,23 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/447ff942-7036-4cac-9d81-f08e1798b577"; }
-    ];
+  fileSystems."/mnt/win10" =
+    {
+      device = "/dev/disk/by-uuid/A40897C408979440";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" "gid=100" "user" "exec" "umask=000" ]; # important: change uid and gid to the right numbers :D
+    };
+
+  fileSystems."/mnt/games" =
+    {
+      device = "/dev/disk/by-uuid/F4462E50462E13C0";
+      fsType = "ntfs-3g";
+      options = [ "rw" "uid=1000" "gid=100" "user" "exec" "umask=000" ]; # important: change uid and gid to the right numbers :D
+    };
+
+
+
+  swapDevices = [ { device = "/dev/disk/by-uuid/447ff942-7036-4cac-9d81-f08e1798b577"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
