10dcf5625bec162c87df58c3d1ed6595e98af85a
Xaiya Schumin <d.schumin@proton.me>
2025-02-05 11:39:26 +0100

    🐛 fixed touchpad for sway
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/desktop/sway/default.nix
+++ modules/home/desktop/sway/default.nix
@@ -101,7 +101,7 @@ in {
             pointer_accel = "-0.7";
           };
 
-          "236:628:PIXA3854:00_093A:0274_Touchpad" = { 
+          "2362:628:PIXA3854:00_093A:0274_Touchpad" = { 
             pointer_accel = "0.2";
             natural_scroll = "enabled"; 
 
