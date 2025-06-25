dcf8b16e94bc75d9c3bf04d800d0a793d76802e1
Xaiya Schumin <d.schumin@proton.me>
2025-06-25 16:54:05 +0200

    ✨ [sway/waybar] finishing touches and seperating modules into own files
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/desktop/sway/packages/waybar/default.nix
+++ modules/home/desktop/sway/packages/waybar/default.nix
@@ -6,13 +6,21 @@ let
 in
 {
 
+  imports = [
+    ./modules/sway.nix
+    ./modules/control_center.nix
+    ./modules/clock.nix
+    ./modules/battery.nix
+    ./modules/mpris.nix
+    ./modules/tray.nix
+  ];
+
   # replace default bar with waybar
   wayland.windowManager.sway.config.bars = mkIf (builtins.elem "sway" windowManagers) [ { command = "${pkgs.waybar}/bin/waybar"; }];
 
   # only add colors and fonts from stylix
   stylix.targets.waybar.addCss = false;
 
-  home.packages = [ pkgs.playerctl ];
   programs.waybar = {
     enable = builtins.elem "sway" windowManagers;
 
@@ -20,123 +28,25 @@ in
       mainBar = {
         layer = "top";
         position = "top";
-        height = 35;
+        fixed-center = true;
 
+        height = 35;
         margin-top = 5;
         margin-left = 5;
         margin-right = 5;
 
         margin-bottom = 3;
 
+
         /* Enable modules in the right positions */
 
-        modules-left = [ "image#nix_trans" "sway/workspaces" ];
-        modules-right = [ "mpris" "pulseaudio" "network" "bluetooth" "battery" "tray" "clock" ];
+        modules-left = [ "image#nix" "sway/workspaces" "sway/window" ];
+        modules-center = [ /* "custom/weather" */ "clock" ];
+        modules-right = [  "mpris" "pulseaudio" "network" "bluetooth"  "tray" "battery" ];
 
-        "image#nix_trans" = {
+        "image#nix" = {
           path = "${./lix.svg}";
           size = 20;
-        };
-
-        /* Show the sway workspaces in your waybar */
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-
-          format = "{icon}";
-
-          format-icons = {
-            "1" = " ";
-            "2" = " ";
-            "3" = " ";
-            "4" = "󰕘 ";
-            "5" = "󱝁 ";
-            "6" = " ";
-            "7" = " ";
-            "8" = " ";
-            "9" = "󱕅 ";
-            "10" = " ";
-            sort-by-number = true;
-          };
-        };
-
-        "bluetooth" = {
-          interval = 3;
-          format-on = "";
-          format-disabled = "󰂲";
-          format-connected = "󰂱 {device_alias} {device_battery_percentage}%";
-
-          on-click = "exec ${pkgs.blueman}/bin/blueman-manager";
-        };
-
-        "tray" = {
-          spacing = 10;
-          icon-size = 18;
-          show-passive-items = true;
-        };
-
-        /* Show the current audio device with icons and change the volume by scrolling */
-        "pulseaudio" = {
-          format = "{icon} {volume}%";
-          on-click = "pavucontrol";
-
-          states.muted = 0;
-          format-muted = "  {volume}%";
-
-          tooltip-format = "Device: {desc}";
-
-          format-icons = [ "" " " ];
-        };
-
-        /* Show the current network type; If the connection is LAN, show bandwith status with IP adress */
-        "network" = {
-          interval = 3;
-          max-length = 30;
-
-          format-wifi = "{icon} {essid}";
-          format-ethernet = "󰌗 ";
-
-          format-icons = [ "󰤟 " "󰤢 " "󰤥 " "󰤨 " ];
-
-        };
-
-        /* The Battery status will be shown */
-        "battery" = {
-          interval = 10;
-          full-at = 80; # Changed in BIOS for better capacity
-
-          tooltip-format = "Watt usage: {power} \nCapacity: {capacity}% /time remain: {time} \nBattery cycles: {cycles}";
-
-          format = "{icon} {capacity}%";
-          format-charging = " {icon} {capacity}%";
-          format-full = "󰚥";
-
-          format-icons = [ "󱟩" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹" ];
-        };
-
-        /* Show the current time (change formating here...) */
-        "clock" = {
-          interval = 60;
-
-          timezone = "Europe/Berlin";
-          locale = "de_DE.UTF-8";
-          format = "󱑅  {:%H:%M}";
-        };
-
-        "mpris" = {
-          interval = 1;
-          max-length = 80;
-
-        	format = "{player_icon} <i>{position}/{length}</i>";
-          format-paused = "{status_icon} <i>{position}/{length}</i>";
-
-        	player-icons = {
-        	  default = " ";
-        	  firefox = " "; # Librewolf is based on firefox
-        	  chromium = "󰤺 "; # TIDAL-HIFI is based on chromium, so will use that
-        	};
-
-        	status-icons.paused = " ";
         };
       };
     };
---
+++ modules/home/desktop/sway/packages/waybar/modules/battery.nix
@@ -0,0 +1,22 @@
+{
+  programs.waybar.settings.mainBar = {
+    /* The Battery status will be shown */
+    "battery" = {
+      interval = 10;
+      full-at = 80; # Changed in BIOS for better capacity
+
+      tooltip-format = "Watt usage: {power} \nCapacity: {capacity}% /time remain: {time} \nBattery cycles: {cycles}";
+
+      states = {
+        warning = 30;
+        critical = 20;
+      };
+
+      format = "{icon} {capacity}%";
+      format-charging = "( {icon}) {capacity}%";
+      format-full = "󰚥";
+
+      format-icons = [ "󱟩" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹" ];
+    };
+  };
+}
