18bd8e47432fc8c93e3adbadc928f7b11c3685be
Xaiya Schumin <d.schumin@proton.me>
2025-07-19 12:59:38 +0200

    🧑‍💻 Added development environemt for flake
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -7,6 +7,7 @@
     ./modules.nix # Inject modules into flake
     ./lib # add my own library
     ./args.nix # Pass arguments used by the flake
+    ./shell.nix # A development environment for this system
 
 
   ];
