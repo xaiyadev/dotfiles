ee3aba910b85639ec8bfdd160e61ae1ac65d8a0d
Xaiya Schumin <d.schumin@proton.me>
2025-09-22 22:34:57 +0200

    ✨🚧 rofi loads now the right configuration -- modes not loading correctly
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/desktop/sway/packages/rofi.nix
+++ modules/home/desktop/sway/packages/rofi.nix
@@ -2,37 +2,43 @@
   pkgs,
   osConfig,
   lib,
+  config,
   ...
 }:
 let
   inherit (lib)
     mkIf
-    concatStringsSep
+    getExe
     ;
 
   sway = osConfig.sylveon.system.graphical.sway;
 in
 {
 
-  programs.rofi = mkIf sway.enable {
-    enable = true;
-    package = pkgs.rofi;
+  config = {
+    programs.rofi = mkIf sway.enable {
+      enable = true;
+      package = pkgs.rofi;
 
-    # Only working when calling rofi from the terminal right now
-    # Refer to https://discourse.nixos.org/t/rofi-on-wayland-and-plugins/17354/8
-    modes = [
-      "combi"
-      "drun" # "emoji"
-    ];
+      # Only working when calling rofi from the terminal right now
+      # Refer to https://discourse.nixos.org/t/rofi-on-wayland-and-plugins/17354/8
+      modes = [
+        "drun"
+        # "ssh"
+        {
+          name = "power";
+          path = getExe pkgs.rofi-power-menu;
+        }
+      ];
 
-    terminal = "${pkgs.kitty}/bin/kitty";
+      plugins = [
+        pkgs.rofi-power-menu
+      ];
+
+      terminal = "${pkgs.kitty}/bin/kitty";
+    };
+
+    wayland.windowManager.sway.config.menu = ''${config.programs.rofi.finalPackage}/bin/rofi -show-icons -show combi'';
   };
-
-  wayland.windowManager.sway.config.menu = concatStringsSep ''\'' [
-    ''${pkgs.rofi}/bin/rofi ''
-    ''-modi power:"${pkgs.rofi-power-menu}/bin/rofi-power-menu" --symbols-font "Symbols Nerd Font Mono" ''
-    ''-combi-modi "drun,power" ''
-    ''-show-icons ''
-    ''-show combi ''
-  ];
 }
+ 
