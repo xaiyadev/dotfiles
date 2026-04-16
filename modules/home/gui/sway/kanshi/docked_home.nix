ac5195f3d20cc0d67d9386b218762513835694fe
Xaiya Schumin <d.schumin@proton.me>
2026-04-16 11:44:07 +0200

    🐛 fixed kanshi layouts
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/gui/sway/kanshi/docked_home.nix
+++ modules/home/gui/sway/kanshi/docked_home.nix
@@ -9,15 +9,6 @@ in
           
         outputs = [
           {
-            criteria = "eDP-2";
-
-
-            mode = "2560x1600@165.000Hz";
-            scale = 1.4;
-            position = "0,0";
-          }
-
-          {
             criteria = "AOC 2460G4 0x0000A8E2";
 
             mode = "1920x1080@119.982Hz";
@@ -29,6 +20,15 @@ in
         
             position = "3750,0";
             mode = "1920x1080@60.000Hz";
+          }
+
+          {
+            criteria = "eDP-2";
+
+
+            mode = "2560x1600@165.000Hz";
+            scale = 1.4;
+            position = "0,0";
           }
         ];
       };
