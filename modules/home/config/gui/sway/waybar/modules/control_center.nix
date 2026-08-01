aba0bc4837a336cae2eb5f6804f18827074cdeb3
Xaiya Schumin <d.schumin@proton.me>
2026-08-01 14:22:18 +0200

    ✨🐛 fixed small waybar problems + added pipewire hi-res support (bit-perfect)
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/config/gui/sway/waybar/modules/control_center.nix
+++ modules/home/config/gui/sway/waybar/modules/control_center.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  inherit (lib) getExe;
+in
 {
   programs.waybar.settings.mainBar = {
 
@@ -8,33 +11,15 @@
       format-disabled = "󰂲 {status}";
       format-connected = "󰂱 {device_alias} {device_battery_percentage}%";
 
-      on-click = "exec ${pkgs.blueman}/bin/blueman-manager";
+      on-click = "exec ${getExe pkgs.overskride}";
     };
 
     # Show the current audio device with icons and change the volume by scrolling
     "pulseaudio" = {
       format = "{icon} {volume}%";
-      on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
+      on-click = "${getExe pkgs.pavucontrol}";
 
       format-icons = [ " " ];
     };
-
-    # Show the current network type; If the connection is LAN, show bandwith status with IP adress
-    "network" = {
-      interval = 3;
-      max-length = 30;
-
-      format-wifi = "{icon} {essid}";
-      format-ethernet = "󰌗 ";
-
-      format-icons = [
-        "󰤟 "
-        "󰤢 "
-        "󰤥 "
-        "󰤨 "
-      ];
-
-    };
-
   };
 }
--- modules/nixos/hardware/audio.nix
+++ modules/nixos/hardware/audio.nix
@@ -12,12 +12,10 @@ in
 {
   config = mkIf prof.graphical.enable {
     sylveon.packages = {
-      inherit (pkgs)
-        qpwgraph
-        pwvucontrol
-        ;
+      inherit (pkgs) pwvucontrol;
     };
 
+    # we dont wanna use pulse audio, only pipewire
     services.pulseaudio.enable = lib.mkForce false;
 
     services.pipewire = {
@@ -26,39 +24,29 @@ in
       audio.enable = true;
       pulse.enable = true;
       jack.enable = true;
+
       alsa = {
-        enable = true; # Alsa very nice for exclusive mode
+        enable = true;
         support32Bit = true;
       };
 
       extraLadspaPackages = [ pkgs.rnnoise-plugin ];
-
+      
       extraConfig.pipewire = {
-        "10-loopback" = {
-          "context.modules" = [
-            {
-              "node.description" = "playback loop";
-              "audio.position" = [
-                "FL"
-                "FR"
-              ];
+        # configuration for my FiiO Btr5
+        "hires" = {
+          "context.properties" = {
+            "default.clock.rate" = 44100;
+            "default.clock.allowed-rates" = [ 32000 64000 128000 44100 48000 88200 96000 176400 192000 352800 384000 ];
+          };
+          
+          "stream.properties" = {
+            "resample.quality" = 14;
+          };  
 
-              "capture.props" = {
-                "node.name" = "playback_sink";
-                "node.description" = "playback-sink";
-                "media.class" = "Audio/Sink";
-              };
-
-              "playback.props" = {
-                "node.name" = "playback_sink.output";
-                "node.description" = "playback-sink-output";
-                "media.class" = "Audio/Source";
-                "node.passive" = true;
-              };
-            }
-          ];
         };
       };
+
     };
 
     systemd.user.services = {
