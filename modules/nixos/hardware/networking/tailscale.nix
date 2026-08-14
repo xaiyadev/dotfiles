f230f16afb9c6f94ffc54e96842ca90196ae369e
Xaiya Schumin <d.schumin@proton.me>
2026-08-14 13:46:18 +0200

    🚑️ added dnsmasq waiting for tailscale to start
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/nixos/hardware/networking/tailscale.nix
+++ modules/nixos/hardware/networking/tailscale.nix
@@ -25,47 +25,52 @@ in
 
     upFlags = mkOption {
       type = (listOf string);
-      default = [ "--ssh" ];
+      default = [ "" ];
       description = ''
         Which up flags to insert into tailscale      
       '';
     };
   };
 
-  config = mkIf cfg.enable {
-    networking.firewall = {
-      trustedInterfaces = [ "${tailscale.interfaceName}" ];
-      checkReversePath = "loose";
-
-      allowedUDPPorts = [ tailscale.port ];
-    };
-
-    services = mkMerge [
-      {
-        tailscale = {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      networking.firewall = {
+        trustedInterfaces = [ "${tailscale.interfaceName}" ];
+        checkReversePath = "loose";
+      
+        allowedUDPPorts = [ tailscale.port ];
+      };
+      
+      services.tailscale = {
           enable = true;
-          
+            
           permitCertUid = "root";
           useRoutingFeatures = if cfg.isServer then "server" else "client";
-          
-          extraUpFlags = cfg.upFlags ++ [ "--advertise-exit-node" ];
+            
+          extraUpFlags = cfg.upFlags ++ [ "--ssh" ];
         };
-      }
+    }
 
-      (mkIf cfg.isServer { # TODO move?
-        # add a dnsmasq for the tailscale network
-        dnsmasq = {
-          enable = true;
-        
-          settings = {
-            listen-address = [ "100.111.243.3" ]; # apricot (server ip) should this be set as an option? TODO
-            bind-interfaces = true;
-        
-            address = [ "/xaiya.dev/100.111.243.3" ];
-            no-resolv = true;
-          };
+    (mkIf cfg.isServer { # TODO move?
+      # add a dnsmasq for the tailscale network
+      services.dnsmasq = {
+        enable = true;
+          
+        settings = {
+          listen-address = [ "100.111.243.3" ]; # apricot (server ip) should this be set as an option? TODO
+          bind-interfaces = true;
+          
+          address = [ "/xaiya.dev/100.111.243.3" ];
+          no-resolv = true;
         };
-      })
-    ];
-  };
+      };
+
+      systemd.services.dnsmasq = {
+        after = [ "tailscaled.service" ];
+        requires = [ "tailscaled.service" ];
+      };
+    })
+
+
+  ]);
 }
