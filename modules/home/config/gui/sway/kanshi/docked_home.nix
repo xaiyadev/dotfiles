378deb4e6ede6732f6844b40b6be2266eb5a2a0d
Xaiya Schumin <d.schumin@proton.me>
2026-09-05 20:10:10 +0200

    ✨ added new monitor to home setp and configured
    Signed-off-by: Xaiya Schumin <d.schumin@proton.me>

--- modules/home/config/gui/sway/kanshi/docked_home.nix
+++ modules/home/config/gui/sway/kanshi/docked_home.nix
@@ -9,18 +9,25 @@ in
 
         outputs = [
           {
+            criteria = "AOC CQ32G4 RK3S5JA005767";
+            position = "2327,0";
+
+            mode = "2560x1440@144.001Hz";
+          }
+
+          {
             criteria = "Acer Technologies RT240Y T75EE0042411";
-            position = "1831,0";
+            position = "4887,360";
 
             mode = "1920x1080@60.000Hz";
           }
 
           {
-             criteria = "eDP-2";
-             position = "0,0";
+            criteria = "eDP-2";
+            position = "0,0";
 
-             mode = "2560x1600@165.000Hz";
-             scale = 1.4;
+            mode = "2560x1600@165.000Hz";
+            scale = 1.1;
           }
 
         ];
